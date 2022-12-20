79c554532cb63e36128d37e88cfc53282b68f96e
aastom <astom.ahashie@bigspark.dev>
2022-12-20 18:17:30 +0000

    add git ignore

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 fruits = pandas.read_csv(
     "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
 )
-streamlit.datafram(fruits)
+streamlit.dataframe(fruits)
