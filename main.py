8e6ceaa870c44be212ee7bf6e6f3d1be61d58d28
aastom <astom.ahashie@bigspark.dev>
2023-01-03 12:21:21 +0000

    add fruityvice

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ fruits = pandas.read_csv(
     "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
 )
 
-# Let's put a pick list here so they can pick the fruit they want to include
+# Let's put a pick list here, so they can pick the fruit they want to include
 fruits = fruits.set_index("Fruit")
 fruits_selected = streamlit.multiselect(
     "Pick some fruits:", list(fruits.index), ["Avocado", "Strawberries"]
@@ -20,3 +20,17 @@ fruits_selected = streamlit.multiselect(
 fruits_to_show = fruits.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+# New Section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+import requests
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+streamlit.dataframe(fruityvice_normalized)
